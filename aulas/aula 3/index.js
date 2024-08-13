7fdb3f58c884f8f646c98c6107d9fe6277f51120
bruna <bruna.gpires@hotmai.com>
2024-08-13 17:26:13 -0300

    aula 4

---
+++ aulas/aula 3/index.js
@@ -0,0 +1,93 @@
+// texto exemplo
+
+/* texto comentado
+linha 1
+linha 2
+linha 3 */
+
+console.log("ola mundo!")
+console.log("é os guri")
+
+prompt("qual seu nome ?")
+
+const nome = "luis"
+const idade = 18
+
+console.log("olá","Meu nome é:", nome," e eu tenho:", idade,"anos")
+
+
+const sobrenome = "santarem"
+
+const estudante = "curso"
+
+console.log("olá","Meu nome é:",nome,"e meu sobrenome",sobrenome,"e eu tenho",idade,"ano","faço",estudante,"no senac")
+
+const got = "Game of Thrones"
+
+const temporadasDeGot = "8";
+
+console.log(typeof got)
+console.log(typeof temporadasDeGot)
+
+let novaVariavel
+console.log(typeof novaVariavel)
+
+novaVariavel = 2
+console.log(typeof novaVariavel)
+
+novaVariavel = undefined
+console.log(typeof novaVariavel)
+
+const novonome = prompt("qual seu nome");
+console.log( novonome)
+
+const novoidade = prompt("qual sua idade");
+
+console.log(novoidade)
+
+console.log (typeof novonome) 
+
+console.log(typeof novoidade)
+
+const idadeNumero = 23
+
+const idadeTexto = idadeNumero.tostring()
+
+console.log(typeof idadeNumero)
+
+console.log(typeof idadeTexto)
+
+const idadeTexto2 = "23"
+const idadeNumero2 = Number()(idadeTexto2)
+
+console.log(typeof idadeTexto2)
+
+console.log(typeof idadeNumero2)
+if (n % 2 == 0)
+    return 'par!'
+else { }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ aulas/trabalho avaliativo/index.js
@@ -0,0 +1,32 @@
+// trabalho avaliaitvo, vamos coverter graus celsius para fahrenheit
+//vamos fazer com console.log e prompt
+/*
+
+Celsius          fahrenheit        o que o programa vai responder
+  38                100,4                     100,4
+  23                 73,4                      73,4
+  12                 53,6                      53,6
+  7                  44,6                      44,6
+ -28                -18,4                     -18,4
+ -13                  8,6                       8,6
+numero de tentativas:
+1. na primeira tentativa tentamos fazer o de impar ou par mas acabou que achamos mas facil fazer em celsius.
+2. acabamos trocando cons por let e tivemos que trocar denovo.
+3. acabamos esquecendo de por as aspas nas palavras, e não estavamos conseguindo achar, ficamos um tempinho procurando.
+4. acabei esquecendo de botar o + 32 para dar o resultado, acabando não respondendo nossas questões.
+ 
+solução
+
+acabamos indo no google ou indo no arquivo do drive e tambem pesquisando no youtube, e acabamos achando respostas.
+
+nosso algoritmo falhou 3 vezes, mas na ultiam tentativa funcionou.
+
+*/
+let celsius
+let fahrenheit
+
+celsius = prompt("qual e o grau celsius?")
+console.log("o numero fica em celsius ",celsius)
+fahrenheit = (celsius *1.8) + 32
+console.log("em fahrenheit ficara", fahrenheit)
+
