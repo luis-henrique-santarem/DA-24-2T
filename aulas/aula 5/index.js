2463cd03b77aecaf09128bf2315684c0642541bf
bruna <bruna.gpires@hotmai.com>
2024-08-19 14:02:39 -0300

    aula 5

---
+++ aulas/aula 5/index.js
@@ -0,0 +1,97 @@
+/*
+let a = 10  variavel "a" inserindo um numero 10
+
+let b = 10  variavel "b" inserindo um numero 10
+
+console.log(b) impressão do console
+
+b = 5 equivalente da variavel "b" sendo trocado por outro numero
+
+ console.log(a, b)
+
+  a função de codigo e tranformar á variavel "b" de 10 para 5.
+*/
+
+
+/*
+let a = 10 variavel a inserindo numero 10.
+
+let b = 20 variavel b inserindo numero 20.
+
+c=a  o valor de "c" agora tera o mesmo valor de "a".
+
+b=c o valor de "b" agora tera o mesmo valor de "c".
+
+a=b o valor de "a" agora tera o mesmo valor de "b".
+
+console.log(a, b ,c ) tornando todos com a mesma resposta "a" "b" "c".
+*/
+
+
+/*
+
+let horasTrabalhadasPorDia = prompt("quantas horas voce trabalha por dia ? ") usa uma variavel para saber quantas horas trablha usando o "prompt" 
+let salarioDiario = prompt("quantos voce recebe por dia?") usa uma variavel para saber quanto ela recebe usando "prompt"
+console.log("voce receb",salarioDiario,"/",horasTrabalhadasPorDia,"por hora") o console.log daria o resultado
+
+horasTrabalhadasPorDia: Representa a quantidade de horas que o usuário trabalha por dia.
+salarioDiario: Representa o valor que o usuário recebe por dia.
+
+
+*/
+
+//1. declare uma variavel para armazenar um nome, sem atribuir  um valor.
+
+//let nome;
+
+//2. declare uma variavel para armazenar uma idade, sem atribuir um valor.
+
+//let idade;
+
+//3. imprima na tela i tipo dessas variaveis que acabou de criar, usando o comando typeof.
+
+//console.log(typeof nome); // deve imprimir "undefined"
+//console.log(typeof idade); // deve imprimir "undefined"
+
+/*
+4. por que esse tipo foi impresso?
+as variaveis 'nome' e 'idade' foram declaradas, mas não foram inicializados com valores. portanto, o tipo impresso é "undefined"
+*/
+
+//5.pergunte ao usuario seu nomee sau idade, atribuindo esses dois valores as variaveis que acabou de criar.
+
+//nome = prompt("qual é o seu nome");
+//idade = prompt("qual é a sua idade");
+
+//imprima  na tela o tipo dessas variaveis.
+//console.log(typeof nome);  // deve imprimir "string"
+//console.log(typeof idade); // deve imprimir "string"
+
+/*
+7.para finalizar, imprima na tela a mensagem "ola 'nome', voce tem idade anos". onde 'nome' e 'idade' são os valores que o usuario inseriu.
+
+*/
+
+//console.log('olá ${nome}, voce tem ${idade} anos.')
+
+
+ //Perguntas
+
+ 
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
